df29df4f473d00d362b03a79b153fea1aa146fec
Lucas <lucas@agtlucas.com>
2015-12-27 10:25:04 -0200

    Bump version

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const path = require('path');
-const fs = require('fs');
+'use strict'
+
+const path = require('path')
+const fs = require('fs')
 
 fs.readFile(
   path.resolve(__dirname, 'lucas.json'), 'utf8',
   (err, data) => console.log(err || data)
-);
+)
